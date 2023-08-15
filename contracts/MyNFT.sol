28b4552fc0182374de91ad73734db8acc8dcd9d7
Utkarsh Pandey <theutkarshmail@gmail.com>
2023-08-15 16:48:48 +0530

    "new commit"

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -2,40 +2,28 @@
 pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyNFT is ERC721Enumerable {
-    address public owner;
+contract MyNFT is ERC721Enumerable,Ownable {
+    
     string[] public promptDescription;
 
-    // Base URL for the NFTs
-    string public baseUrl = "https://orange-wonderful-catshark-766.mypinata.cloud/ipfs/QmZo7uw4fqz41HTrLfsi8RS8b9cKXfKz8W7QVoSGp2AA8S/?_gl=1*hq93ov*_ga*NDk0NTY1MjM0LjE2ODgzODEzNzE.*_ga_5RMPXG14TE*MTY5MDczMDc4NC40LjEuMTY5MDczMjUyOS42MC4wLjA.";
+    
+    string public baseUrl = "ipfs://bafybeidmme6am4y47ikr6keqr3cwh4q7psjnquiu3sieeuxe5rhpefszwm/";
 
-    constructor() ERC721("MyNFT", "MNFTT") {
-        owner = msg.sender;
+    constructor() ERC721("Utkarsh_NFT", "UTK") {
+        promptDescription = ["naruto in nazi germany",
+                                "an eagle warrior",
+                                "aliens on mars",
+                                "pikachu on mars",
+                                "shiba doge performing ballet"];
 
-        // Initialize the prompt descriptions by the Pinta !
-        promptDescription = [
-            "house at the middle of the ocean with an extremely luxurious glass and pastel walled villa surrounding a huge jungle at night",
-            "A shiba doge playing electric guitar on top of The Berlin Wall",
-            "Cool motorcyclist with space suit riding on moon with storm and galaxy in background",
-            "Monkey with sunglasses in the woods",
-            "okaybear with sunglasses in the woods"
-        ];
-
-        // Mint the initial NFTs
         for (uint256 i = 0; i < promptDescription.length; i++) {
             _mint(msg.sender, i + 1);
         }
     }
 
-    // Modifier that only allows the owner to execute a function ! 
-    modifier ownerAllowed() {
-        require(msg.sender == owner, "Only the owner can perform this action!");
-        _;
-    }
-
-    // Function to mint NFTs, which only the owner can perform
-    function mint(uint256 quantity) external payable ownerAllowed() {
+    function mint(uint256 quantity) external payable onlyOwner {
         require(quantity <= 5, "You can only mint up to 5 NFTs at a time");
         require(totalSupply() + quantity <= 100, "Maximum NFT supply reached");
 
@@ -45,12 +33,12 @@ contract MyNFT is ERC721Enumerable {
         }
     }
 
-    // Override the baseURI function to return the base URL for the NFTs
+    
     function _baseURI() internal view override returns (string memory) {
         return baseUrl;
     }
 
-    // Return the prompt descriptions
+    
     function getPromptDescriptions() external view returns (string[] memory) {
         return promptDescription;
     }
